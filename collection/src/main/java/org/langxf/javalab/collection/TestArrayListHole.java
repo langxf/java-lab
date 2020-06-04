24a5ef11fd26c83a34f18bb8781e19896374c251
郎晓枫 <langxiaofeng@miaozhen.com>
2020-06-04 21:25:38 +0800

    update

---
+++ collection/src/main/java/org/langxf/javalab/collection/TestArrayListHole.java
@@ -0,0 +1,9 @@
+package org.langxf.javalab.collection;
+
+public class TestArrayListHole {
+
+    public static void main(String args[]) {
+        System.out.println("-------TestArrayListHole------>>>");
+    }
+
+}
