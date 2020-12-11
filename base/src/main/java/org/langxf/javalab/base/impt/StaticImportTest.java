cdfe83d99b37add52b1556a3f92cc3dd404ffbc2
langxf <langxiaofeng2008@163.com>
2020-12-11 21:30:27 +0800

    静态导入类时，被导入类所有方法均需为静态方法

---
+++ base/src/main/java/org/langxf/javalab/base/impt/StaticImportTest.java
@@ -0,0 +1,14 @@
+package org.langxf.javalab.base.impt;
+
+// 静态导入
+import static org.langxf.javalab.base.util.StaticImportUtils.*;
+
+/**
+ * @author Langxf
+ * @date 2020/12/11
+ */
+public class StaticImportTest {
+    public static void main(String[] args) {
+        System.out.println(add(10, 20));
+    }
+}
---
+++ base/src/main/java/org/langxf/javalab/base/util/StaticImportUtils.java
@@ -0,0 +1,14 @@
+package org.langxf.javalab.base.util;
+
+/**
+ * @author Langxf
+ * @date 2020/12/11
+ * 用于静态导入测试，需满足所有方法均为静态方法
+ */
+public class StaticImportUtils {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
