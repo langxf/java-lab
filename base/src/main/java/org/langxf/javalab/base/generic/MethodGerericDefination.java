207acc57b5d9f521bbe9c207643064051367b945
langxf <langxiaofeng2008@163.com>
2020-12-11 11:10:27 +0800

    泛型方法定义示例

---
+++ base/src/main/java/org/langxf/javalab/base/generic/MethodGerericDefination.java
@@ -0,0 +1,32 @@
+package org.langxf.javalab.base.generic;
+
+/**
+ * @author Langxf
+ * @date 2020/12/11
+ * 泛型方法
+ */
+public class MethodGerericDefination {
+    public static void main(String[] args) {
+        Integer[] intArr = fun(1,2,3);
+        for (int data : intArr) {
+            System.out.print(data + ",");
+        }
+
+        System.out.println();
+
+        String[] strArr = fun("hello", "mapleleave,", "this", "is", "generic", "method.");
+        for (String data : strArr) {
+            System.out.print(data + " ");
+        }
+    }
+
+    /**
+     * 泛型方法
+     * @param args
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] fun(T ... args) {
+        return args;
+    }
+}
